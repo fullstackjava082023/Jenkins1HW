8ccbf849e889051f44c0a4634089a8c1b3442c24
iliap <iliap@sapiens.com>
2024-09-24 18:04:25 +0300

    new line to print day of the week

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import datetime
 current_date = datetime.datetime.now()
 
 print("Current date and time: ", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+print("Current day of the week: ", current_date.strftime("%A"))
